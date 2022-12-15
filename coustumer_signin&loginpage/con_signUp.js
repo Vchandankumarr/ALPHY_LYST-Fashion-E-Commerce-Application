27d93b01137f67ddf444dded839ce54add271d5a
Sakshi0704 <banichoudhary0704@gmail.com>
2022-12-15 19:24:41 +0530

    completed-day3 work

---
+++ coustumer_signin&loginpage/con_signUp.js
@@ -0,0 +1,33 @@
+
+let signup_button = document.querySelector("#SignUp_page form");
+
+signup_button.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    let all_input = document.querySelectorAll("#SignUp_page input");
+    let obj = {};
+    for(let i=0;i<all_input.length-1;i++){
+        obj[all_input[i].id]=all_input[i].value;
+    }
+    if(obj)
+    {
+      userdetails(obj);
+    }
+});
+
+async function userdetails(obj){
+    try {
+        let res = await fetch("https://639aeab431877e43d67b3d7d.mockapi.io/users",{
+            method : "POST",
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify(obj),
+        })
+        if(res.ok){
+            console.log(await res.json());
+            alert("Sign Up is seccussful");
+        }
+    } catch (error) {
+        alert("something is wrong");
+    }
+}
