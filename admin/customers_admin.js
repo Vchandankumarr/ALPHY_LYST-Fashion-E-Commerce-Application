23f4e798f59bd6eaf0b16fda7b721f513f591839
Sakshi0704 <banichoudhary0704@gmail.com>
2022-12-17 21:57:33 +0530

    commplited

--- admin/customers_admin.js
+++ admin/customers_admin.js
@@ -1,7 +1,10 @@
 // let button = document.querySelector("#customers_button");
 // button.addEventListener("click",getdata());
+window.addEventListener("load", () => {
+    getdata();
+});
 
-(async function getdata(){
+async function getdata(){
     try {
         let res = await fetch("https://639aeab431877e43d67b3d7d.mockapi.io/users",{
            method: "GET",
@@ -15,25 +18,52 @@
              display(out);
         }
     } catch (error) {
-      alert(error);
+      //alert(error);
     }
-})();
+};
 
 function display(data){
-    //console.log(data);
+   // console.log(data);
     let tableTbodyContainer = document.querySelector("#right-side tbody");
     tableTbodyContainer.innerHTML = "";
 
     let new_formed_data = data.map((elem)=>{
         return `
         <tr>
-            <td>${elem.firstName+" "+elem.lastName}</td>
+            <td>${elem.firsName+" "+elem.lastName}</td>
             <td>${elem.emailId}</td>
             <td>${elem.password}</td>
             <td>${elem.contectNo}</td>
-            <td class="remove">Remove</td>
+            <td class="remove" data-id=${elem.id}>Remove</td>
          </tr>
          `
     });
     tableTbodyContainer.innerHTML = new_formed_data.join(" ");
+
+    // for removeing userer.....
+    let remove_button = document.querySelectorAll(".remove");
+   // console.log(remove_button);
+    for(let btn of remove_button){
+        btn.addEventListener("click",(event)=>{
+            let data_id = event.target.dataset.id;
+           // console.log(data_id);
+           remove(data_id);
+        })
+    }
+}
+
+async function remove(id){
+    try {
+        let remove_data = await fetch(`https://639aeab431877e43d67b3d7d.mockapi.io/users/${id}`,{
+            method : "DELETE",
+            headers : {
+                "Content-Type": "application/json",
+            },
+        });
+        if(remove_data.ok){
+            getdata();
+        }
+    } catch (error) {
+        alert(error);
+    }
 }
